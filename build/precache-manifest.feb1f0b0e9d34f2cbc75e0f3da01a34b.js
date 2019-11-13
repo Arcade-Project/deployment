243d727914364feab6911dd73315d337bf61450f
Diego Pronesti <dieh.diego@gmail.com>
2019-11-13 17:46:27 -0300

    initial commit

---
+++ build/precache-manifest.feb1f0b0e9d34f2cbc75e0f3da01a34b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1608e3aa5e69979fb2f59e2e32e939",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75d0169452450448eb99",
+    "url": "/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "c4709a610c48037c06f4",
+    "url": "/static/css/main.1d7093d4.chunk.css"
+  },
+  {
+    "revision": "75d0169452450448eb99",
+    "url": "/static/js/2.b5468d06.chunk.js"
+  },
+  {
+    "revision": "c4709a610c48037c06f4",
+    "url": "/static/js/main.0e96233b.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  }
+]);
