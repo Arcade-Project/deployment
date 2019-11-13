3008dac827294664fb3dd5306a0a9d43102fb08d
Diego Pronesti <dieh.diego@gmail.com>
2019-11-13 18:03:37 -0300

    working

--- app.js
+++ app.js
@@ -43,10 +43,10 @@ app.use('/score', score);
 // Serve static assets if in production
 
   //set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 
 
