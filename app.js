88637c3106fe1f94d1d2d37d1435638b31f3b9bb
Diego Pronesti <dieh.diego@gmail.com>
2019-11-13 17:52:49 -0300

    serve react app

--- app.js
+++ app.js
@@ -41,13 +41,13 @@ app.use('/user', users);
 app.use('/score', score);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+
   //set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+
 
 module.exports = app
